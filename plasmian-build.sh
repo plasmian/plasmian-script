c64b3b0b603759326e9fc5e780b1664e16a8b23e
eyekay <53867511+Azl-Eyekay@users.noreply.github.com>
2022-03-18 20:19:59 +0530

    Update plasmian-build.sh

--- plasmian-build.sh
+++ plasmian-build.sh
@@ -29,18 +29,18 @@ blame_internet
 rm winehq.key
 
 #Lutris key
-curl https://download.opensuse.org/repositories/home:/strycore/Debian_11/Release.key | sudo apt-key add -
+curl https://download.opensuse.org/repositories/home:/strycore/Debian_Unstable/Release.key > /etc/apt/trusted.gpg.d/lutris.asc
 blame_internet
 
 #Repo modification
 if [[ "$free" = "2" ]]
 then
-  printf "deb http://deb.debian.org/debian testing main contrib non-free\ndeb-src http://deb.debian.org/debian testing main contrib\ndeb https://dl.winehq.org/wine-builds/debian/ bookworm main\ndeb http://download.opensuse.org/repositories/home:/strycore/Debian_11/ ./" > /etc/apt/sources.list
+  printf "deb http://deb.debian.org/debian sid main contrib non-free\ndeb-src http://deb.debian.org/debian sid main contrib\ndeb https://dl.winehq.org/wine-builds/debian/ bookworm main\ndeb http://download.opensuse.org/repositories/home:/strycore/Debian_Unstable/ ./" > /etc/apt/sources.list
   apt update
   blame_internet
 elif [ "$free" = "3" ]
 then
-  printf "deb http://deb.debian.org/debian testing main contrib non-free\ndeb-src http://deb.debian.org/debian testing main contrib\ndeb https://dl.winehq.org/wine-builds/debian/ bookworm main\ndeb http://download.opensuse.org/repositories/home:/strycore/Debian_11/ ./" > /etc/apt/sources.list
+  printf "deb http://deb.debian.org/debian sid main contrib non-free\ndeb-src http://deb.debian.org/debian sid main contrib\ndeb https://dl.winehq.org/wine-builds/debian/ bookworm main\ndeb http://download.opensuse.org/repositories/home:/strycore/Debian_Unstable/ ./" > /etc/apt/sources.list
   apt update
   blame_internet
   apt install -y linux-headers-amd64
@@ -49,7 +49,7 @@ then
   blame_internet
 elif [ "$free" = "1" ]
 then
-  printf "deb http://deb.debian.org/debian testing main\ndeb-src http://deb.debian.org/debian testing main\ndeb https://dl.winehq.org/wine-builds/debian/ bookworm main\ndeb http://download.opensuse.org/repositories/home:/strycore/Debian_11/ ./" > /etc/apt/sources.list
+  printf "deb http://deb.debian.org/debian sid main\ndeb-src http://deb.debian.org/debian sid main\ndeb https://dl.winehq.org/wine-builds/debian/ bookworm main\ndeb http://download.opensuse.org/repositories/home:/strycore/Debian_Unstable/ ./" > /etc/apt/sources.list
   apt update
   blame_internet
   apt install -y vrms
@@ -64,25 +64,24 @@ apt autoremove goldendict dragonplayer juk k3b firefox-esr chromium epiphany-bro
 # if that desn't work (doesn't seem very reliable?) remove packages individually...
 apt upgrade -y
 blame_internet
-apt install -y bash-completion flatpak neofetch kcharselect kamoso vlc elisa kolourpaint command-not-found grub-theme-breeze breeze-gtk-theme sddm-theme-breeze plymouth-theme-breeze winetricks wine-binfmt lutris plasma-discover-backend-flatpak plasma-systemmonitor
+apt install -y bash-completion flatpak snapd neofetch kcharselect kamoso vlc elisa kolourpaint command-not-found grub-theme-breeze breeze-gtk-theme sddm-theme-breeze plymouth-theme-breeze winetricks wine-binfmt lutris plasma-discover-backend-flatpak plasma-systemmonitor
 blame_internet
 apt-file update
 blame_internet
 update-command-not-found
+curl -L https://github.com/TheAssassin/AppImageLauncher/releases/download/v2.2.0/appimagelauncher_2.2.0-travis995.0f91801.bionic_amd64.deb -o /tmp/
+apt install -y ./tmp/appimagelauncher_2.2.0-travis995.0f91801.bionic_amd64.deb
 
 #Flatpak setup
 flatpak remote-add flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak remote-add flathub-beta https://flathub.org/beta-repo/flathub-beta.flatpakrepo
-flatpak install -y --noninteractive org.mozilla.firefox org.onlyoffice.desktopeditors com.usebottles.bottles org.mozilla.Thunderbird com.github.tchx84.Flatseal io.github.prateekmedia.appimagepool
+flatpak install -y --noninteractiveorg.onlyoffice.desktopeditors com.usebottles.bottles org.mozilla.Thunderbird com.github.tchx84.Flatseal io.github.prateekmedia.appimagepool
 blame_internet
 
 #Download plasmian-files
 curl -L https://github.com/plasmian/plasmian-files/archive/refs/heads/main.zip -o /tmp/
 blame_internet
 unzip /tmp/master.zip -d /tmp/
-#Install Firefox config
-mkdir -p /etc/skel/.var/app/org.mozilla.firefox/config/fontconfig/
-cp /tmp/plasmian-files/fonts.conf /etc/skel/.var/app/org.mozilla.firefox/config/fontconfig/
 #Install custom app names
 mkdir -p /etc/skel/.local/share/applications
 cp /tmp/files/* /etc/skel/.local/share/applications
@@ -99,3 +98,4 @@ update-grub
 #tweaks
 kwriteconfig5 --file kdeglobals --group "KDE" --key 'SingleClick' 'false'
 kwriteconfig5 --file plasma-org.kde.plasma.desktop-appletsrc --group "Containments" --group "1" --group "General" --key 'arrangement' 1
+
